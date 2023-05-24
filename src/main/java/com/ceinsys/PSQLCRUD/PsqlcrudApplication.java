e2ab0be67acd4102ca6abe0d2f63f6f5ec88489b
Shoaib Ansari <shoaibshamimansari@gmail.com>
2023-05-24 10:43:22 +0530

    Inital commit

---
+++ src/main/java/com/ceinsys/PSQLCRUD/PsqlcrudApplication.java
@@ -0,0 +1,13 @@
+package com.ceinsys.PSQLCRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsqlcrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsqlcrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ceinsys/PSQLCRUD/PsqlcrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ceinsys.PSQLCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsqlcrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
