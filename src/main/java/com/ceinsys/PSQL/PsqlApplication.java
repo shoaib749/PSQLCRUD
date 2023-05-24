a03172bffdf50457cc961d2c60a4e385eb5cd0bd
Shoaib Ansari <66304849+shoaib749@users.noreply.github.com>
2023-05-24 17:20:59 +0530

    Add files via upload

---
+++ src/main/java/com/ceinsys/PSQL/PsqlApplication.java
@@ -0,0 +1,13 @@
+package com.ceinsys.PSQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsqlApplication.class, args);
+	}
+
+}
